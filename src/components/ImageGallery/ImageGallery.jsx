924ad2403cb69916b54ce2015849dc56e84bfddd
liubov12 <68028382+Liubov12@users.noreply.github.com>
2022-06-25 13:28:44 +0200

    image-finder

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import styles from './ImageGallery.module.css';
+const ImageGallery = ({ images, openModal }) => {
+  return (
+    <ul className={styles.ImageGallery}>
+      {images.map(image => (
+        <ImageGalleryItem openModal={openModal} key={image.id} image={image} />
+      ))}
+    </ul>
+  );
+};
+
+export default ImageGallery;
